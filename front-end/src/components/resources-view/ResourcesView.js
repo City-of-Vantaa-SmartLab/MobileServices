63b5c68086fa2b0f9403fcf25ac38116be008b5a
Mariia Ustilkina <mariia.ustilkina@gofore.com>
2018-09-14 00:07:37 +0300

    Create draft structure

---
+++ front-end/src/components/resources-view/ResourcesView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import NavigationPanel from './../navigation/NavigationPanel';
+
+
+class ResourcesView extends Component {
+    
+
+    render() {
+        return (
+            <div>
+             Resources are here
+             <NavigationPanel/>
+            </div>
+        )
+    }
+}
+
+export default ResourcesView;
---
+++ front-end/src/components/settings-view/SettingsView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import NavigationPanel from './../navigation/NavigationPanel';
+
+
+class SettingsView extends Component {
+    
+
+    render() {
+        return (
+            <div>
+             Settings are here
+             <NavigationPanel/>
+            </div>
+        )
+    }
+}
+
+export default SettingsView;
