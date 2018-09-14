bf7e8489c0bce0bb3e2b981436b69bc01ba2d8a3
biraj.paul <biraj.paul@gofore.com>
2018-09-14 13:15:36 +0300

    Refactoring of code
    Moved Navigation Panel to App.js so that it doesn't need to be rendered with every sub-view, and added redirect to newsfeed for /

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,23 +1,24 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import logo from "./../assets/images/logo.svg";
+import { Route, Switch, Redirect } from 'react-router-dom';
+// import logo from "./../assets/images/logo.svg";
 import "./../assets/stylesheets/App.css";
 
 import NewsfeedView from './newsfeed-view/NewsfeedView';
 import ResourcesView from './resources-view/ResourcesView';
 import SettingsView from './settings-view/SettingsView';
+import NavigationPanel from "./navigation/NavigationPanel";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-            <Switch>
-              <Route path="/newsfeed" component={NewsfeedView} />
-              <Route path="/resources" component={ResourcesView} />
-              <Route path="/settings" component={SettingsView} />
-            </Switch>
-        </Router>
+          <Switch>
+            <Route path="/newsfeed" component={NewsfeedView} />
+            <Route path="/resources" component={ResourcesView} />
+            <Route path="/settings" component={SettingsView} />
+            <Route exact path="/" render={() => (<Redirect to="/newsfeed"/>)} />
+          </Switch>
+          <NavigationPanel />
       </div>
     );
   }
