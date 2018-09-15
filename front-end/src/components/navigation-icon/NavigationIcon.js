87bccee95911b29e4fff6f2b1741afe0406dd301
biraj.paul <biraj.paul@gofore.com>
2018-09-15 22:13:50 +0300

    Added background and Navigation icons
    Navigation Icons were added with their text. The background is a placeholder for now.

---
+++ front-end/src/components/navigation-icon/NavigationIcon.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { withRouter } from 'react-router'
+import SvgIcon from "../svg-icon/SvgIcon";
+import "../../assets/stylesheets/NavigationIcon.css"
+
+
+class NavigationIcon extends Component {
+
+    render() {
+
+        return (
+            <div className="navigationIcon">
+                <SvgIcon isActive={this.props.match && this.props.match.isExact ? true : false} name={this.props.iconName} />
+                <div>{this.props.iconText}</div>
+            </div>
+        )
+    }
+}
+
+export default withRouter(NavigationIcon);
---
+++ front-end/src/components/svg-icon/SvgIcon.js
@@ -0,0 +1,120 @@
+import React from "react";
+
+const preferencesIcon = (
+    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        {/* Generator: Sketch 51.3 (57544) - http://www.bohemiancoding.com/sketch */}
+        <title>Glyphs / Activity View Action / Contact</title>
+        <desc>Created with Sketch.</desc>
+        <defs></defs>
+        <g id="Glyphs-/-Activity-View-Action-/-Contact" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="settings_48px" fill="#9B9B9B" fillRule="nonzero">
+                <path d="M21.1606843,13.17 C21.2100182,12.786 21.2470186,12.396 21.2470186,12 C21.2470186,11.604 21.2100182,11.214 21.1606843,10.83 L23.7692131,8.844 C24.003549,8.664 24.0713831,8.34 23.9172147,8.076 L21.4505208,3.918 C21.2963525,3.66 20.9756823,3.552 20.6981792,3.66 L17.6271454,4.866 C16.9919717,4.392 16.2951307,3.99 15.5427891,3.684 L15.080284,0.504 C15.0247833,0.222 14.7719472,0 14.4636105,0 L9.53022277,0 C9.22188604,0 8.96904992,0.222 8.91971604,0.504 L8.45721095,3.684 C7.70486932,3.99 7.0080283,4.386 6.37285463,4.866 L3.30182078,3.66 C3.02431772,3.558 2.70364752,3.66 2.54947915,3.918 L0.0827852949,8.076 C-0.0713830713,8.334 -0.00354899015,8.658 0.230786926,8.844 L2.83314895,10.83 C2.78381507,11.214 2.74681466,11.604 2.74681466,12 C2.74681466,12.396 2.78381507,12.786 2.83314895,13.17 L0.230786926,15.156 C-0.00354899015,15.336 -0.0713830713,15.66 0.0827852949,15.924 L2.54947915,20.082 C2.70364752,20.34 3.02431772,20.448 3.30182078,20.34 L6.37285463,19.134 C7.0080283,19.608 7.70486932,20.01 8.45721095,20.316 L8.91971604,23.496 C8.96904992,23.778 9.22188604,24 9.53022277,24 L14.4636105,24 C14.7719472,24 15.0247833,23.778 15.0741172,23.496 L15.5366223,20.316 C16.2889639,20.01 16.985805,19.614 17.6209786,19.134 L20.6920125,20.34 C20.9695155,20.442 21.2901857,20.34 21.4443541,20.082 L23.911048,15.924 C24.0652163,15.666 23.9973823,15.342 23.7630463,15.156 L21.1606843,13.17 Z M12,16 C9.78857143,16 8,14.2114286 8,12 C8,9.78857143 9.78857143,8 12,8 C14.2114286,8 16,9.78857143 16,12 C16,14.2114286 14.2114286,16 12,16 Z" id="Shape"></path>
+            </g>
+        </g>
+    </svg>
+);
+
+const preferencesIconActive = (
+    <svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        {/* Generator: Sketch 51.3 (57544) - http://www.bohemiancoding.com/sketch */}
+        <title>Glyphs / Activity View Action / Contact clicked</title>
+        <desc>Created with Sketch.</desc>
+        <defs></defs>
+        <g id="Glyphs-/-Activity-View-Action-/-Contact-clicked" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="settings_48px" fill="#3C8FDE" fillRule="nonzero">
+                <path d="M21.2470186,12 C21.2470186,11.604 21.2100182,11.214 21.1606843,10.83 L23.7692131,8.844 C24.003549,8.664 24.0713831,8.34 23.9172147,8.076 L21.4505208,3.918 C21.2963525,3.66 20.9756823,3.552 20.6981792,3.66 L17.6271454,4.866 C16.9919717,4.392 16.2951307,3.99 15.5427891,3.684 L15.080284,0.504 C15.0247833,0.222 14.7719472,0 14.4636105,0 L9.53022277,0 C9.22188604,0 8.96904992,0.222 8.91971604,0.504 L8.45721095,3.684 C7.70486932,3.99 7.0080283,4.386 6.37285463,4.866 L3.30182078,3.66 C3.02431772,3.558 2.70364752,3.66 2.54947915,3.918 L0.0827852949,8.076 C-0.0713830713,8.334 -0.00354899015,8.658 0.230786926,8.844 L2.83314895,10.83 C2.78381507,11.214 2.74681466,11.604 2.74681466,12 C2.74681466,12.396 2.78381507,12.786 2.83314895,13.17 L0.230786926,15.156 C-0.00354899015,15.336 -0.0713830713,15.66 0.0827852949,15.924 L2.54947915,20.082 C2.70364752,20.34 3.02431772,20.448 3.30182078,20.34 L6.37285463,19.134 C7.0080283,19.608 7.70486932,20.01 8.45721095,20.316 L8.91971604,23.496 C8.96904992,23.778 9.22188604,24 9.53022277,24 L14.4636105,24 C14.7719472,24 15.0247833,23.778 15.0741172,23.496 L15.5366223,20.316 C16.2889639,20.01 16.985805,19.614 17.6209786,19.134 L20.6920125,20.34 C20.9695155,20.442 21.2901857,20.34 21.4443541,20.082 L23.911048,15.924 C24.0652163,15.666 23.9973823,15.342 23.7630463,15.156 L21.1606843,13.17 C21.2100182,12.786 21.2470186,12.396 21.2470186,12 Z M12,16 C9.78857143,16 8,14.2114286 8,12 C8,9.78857143 9.78857143,8 12,8 C14.2114286,8 16,9.78857143 16,12 C16,14.2114286 14.2114286,16 12,16 Z" id="Shape"></path>
+            </g>
+        </g>
+    </svg>
+);
+
+const resourcesIcon = (
+    <svg width="32px" height="22px" viewBox="0 0 32 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        {/* Generator: Sketch 51.3 (57544) - http://www.bohemiancoding.com/sketch */}
+        <title>Glyphs / Bar Button / Resources / Off</title>
+        <desc>Created with Sketch.</desc>
+        <defs></defs>
+        <g id="Glyphs-/-Bar-Button-/-Resources-/-Off" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="Group-4" transform="translate(1.000000, 1.000000)" stroke="#979797">
+                <path d="M13.7853133,15.8621098 C13.0444772,15.9976047 12.2726447,16.0692248 11.4796256,16.0692248 C9.35792717,16.0692248 7.38788485,15.556558 5.75737297,14.6795603 L0.947731622,16.3636364 L3.18255885,12.6876287 C1.99926869,11.3746583 1.30340699,9.76879686 1.30340699,8.03461239 C1.30340699,3.5972185 5.85945524,0 11.4796256,0 C16.7745494,0 21.1249515,3.1929145 21.6107451,7.273195 C21.5764846,7.27288348 21.5421742,7.27272727 21.5078152,7.27272727 C16.909494,7.27272727 13.1818182,10.0705639 13.1818182,13.5218702 C13.1818182,14.3493679 13.3961096,15.1392999 13.7853133,15.8621098 Z" id="Combined-Shape"></path>
+                <path d="M17.1293674,18.7112124 L13.2457194,20 L14.9437415,17.1009725 C13.9984255,16.0872184 13.4437793,14.8531597 13.4437793,13.5218702 C13.4437793,10.0705639 17.1714551,7.27272727 21.7697763,7.27272727 C26.3680975,7.27272727 30.0957733,10.0705639 30.0957733,13.5218702 C30.0957733,16.9731766 26.3680975,19.7710132 21.7697763,19.7710132 C20.0517748,19.7710132 18.455305,19.3804684 17.1293674,18.7112124 Z" id="Combined-Shape" transform="translate(21.638796, 13.636364) scale(-1, 1) translate(-21.638796, -13.636364) "></path>
+            </g>
+        </g>
+    </svg>
+);
+
+const resourcesIconActive = (
+    <svg width="32px" height="22px" viewBox="0 0 32 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        {/* Generator: Sketch 51.3 (57544) - http://www.bohemiancoding.com/sketch */}
+        <title>Glyphs / Bar Button / Resources / Off Copy</title>
+        <desc>Created with Sketch.</desc>
+        <defs></defs>
+        <g id="Glyphs-/-Bar-Button-/-Resources-/-Off-Copy" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="Group-4" transform="translate(1.000000, 1.000000)" stroke="#3C8FDE">
+                <path d="M13.7258938,15.8621098 C12.988251,15.9976047 12.2197453,16.0692248 11.4301444,16.0692248 C9.31584543,16.0692248 7.3527985,15.5557103 5.72853238,14.6773853 L0.933730858,16.3636364 L3.17278005,12.6920153 C1.99219563,11.3782358 1.29778885,9.77072865 1.29778885,8.03461239 C1.29778885,3.5972185 5.83419896,0 11.4301444,0 C16.7022453,0 21.0338957,3.1929145 21.5175954,7.273195 C21.4834825,7.27288348 21.44932,7.27272727 21.4151091,7.27272727 C16.8366083,7.27272727 13.125,10.0705639 13.125,13.5218702 C13.125,14.3493679 13.3383677,15.1392999 13.7258938,15.8621098 Z" id="Combined-Shape"></path>
+                <path d="M17.0518147,18.7093259 L13.1809136,20 L14.8824841,17.1043685 C13.9393218,16.0899749 13.3858319,14.8546466 13.3858319,13.5218702 C13.3858319,10.0705639 17.0974402,7.27272727 21.675941,7.27272727 C26.2544419,7.27272727 29.9660502,10.0705639 29.9660502,13.5218702 C29.9660502,16.9731766 26.2544419,19.7710132 21.675941,19.7710132 C19.9637381,19.7710132 18.3727704,19.3797345 17.0518147,18.7093259 Z" id="Combined-Shape" transform="translate(21.545525, 13.636364) scale(-1, 1) translate(-21.545525, -13.636364) "></path>
+            </g>
+        </g>
+    </svg>
+);
+
+const newsIcon = (
+    <svg width="19px" height="20px" viewBox="0 0 19 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        {/* Generator: Sketch 51.3 (57544) - http://www.bohemiancoding.com/sketch */}
+        <title>Glyphs / Bar Button / news icon/ unselected</title>
+        <desc>Created with Sketch.</desc>
+        <defs></defs>
+        <g id="Glyphs-/-Bar-Button-/-news-icon/-unselected" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <g id="Group" stroke="#979797">
+                <rect id="Rectangle" x="0.5" y="0.5" width="18" height="19" rx="2"></rect>
+                <rect id="Rectangle" fill="#9B9B9B" x="3.66666667" y="3.65789474" width="4.27777778" height="4.26315789" rx="1"></rect>
+                <path d="M10.0277778,3.68421053 L15.3055556,3.68421053" id="Line" strokeLinecap="square"></path>
+                <path d="M10.0277778,5.51315789 L15.3055556,5.51315789" id="Line" strokeLinecap="square"></path>
+                <path d="M10.0277778,7.61842105 L15.3055556,7.61842105" id="Line" strokeLinecap="square"></path>
+                <path d="M3.69444444,9.73684211 L15.3055556,9.73684211" id="Line" strokeLinecap="square"></path>
+                <path d="M3.69444444,11.7872807 L15.3055556,11.7872807" id="Line" strokeLinecap="square"></path>
+                <path d="M3.69444444,13.997807 L15.3055556,13.997807" id="Line" strokeLinecap="square"></path>
+                <path d="M3.69444444,16.2083333 L15.3055556,16.2083333" id="Line" strokeLinecap="square"></path>
+            </g>
+        </g>
+    </svg>
+);
+
+const newsIconActive = (
+    <svg width="19px" height="20px" viewBox="0 0 19 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        {/* Generator: Sketch 51.3 (57544) - http://www.bohemiancoding.com/sketch */}
+        <title>Glyphs / Bar Button / news icon/ selected</title>
+        <desc>Created with Sketch.</desc>
+        <defs></defs>
+        <g id="Glyphs-/-Bar-Button-/-news-icon/-selected" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+            <rect id="Rectangle" stroke="#3C8FDE" x="0.5" y="0.5" width="18" height="19" rx="2"></rect>
+            <rect id="Rectangle" stroke="#3C8FDE" fill="#3E92CF" x="3.66666667" y="3.65789474" width="4.27777778" height="4.26315789" rx="1"></rect>
+            <path d="M10.0277778,3.68421053 L15.3055556,3.68421053" id="Line" stroke="#3C8FDE" strokeLinecap="square"></path>
+            <path d="M10.0277778,5.51315789 L15.3055556,5.51315789" id="Line" stroke="#3C8FDE" strokeLinecap="square"></path>
+            <path d="M10.0277778,7.61842105 L15.3055556,7.61842105" id="Line" stroke="#3C8FDE" strokeLinecap="square"></path>
+            <path d="M3.69444444,9.73684211 L15.3055556,9.73684211" id="Line" stroke="#3C8FDE" strokeLinecap="square"></path>
+            <path d="M3.69444444,11.7872807 L15.3055556,11.7872807" id="Line" stroke="#3C8FDE" strokeLinecap="square"></path>
+            <path d="M3.69444444,13.997807 L15.3055556,13.997807" id="Line" stroke="#3C8FDE" strokeLinecap="square"></path>
+            <path d="M3.69444444,16.2083333 L15.3055556,16.2083333" id="Line" stroke="#3C8FDE" strokeLinecap="square"></path>
+        </g>
+    </svg>
+);
+
+const getSvg = (name, isActive) => {
+    switch(name) {
+        case 'newsIcon': 
+            return isActive ? newsIconActive : newsIcon;
+        case 'resourcesIcon':
+            return isActive ? resourcesIconActive : resourcesIcon;
+        case 'preferencesIcon':
+            return isActive ? preferencesIconActive : preferencesIcon;
+        default:
+            return null;
+    }
+}
+
+const SvgIcon = (props) => {
+    return getSvg(props.name, props.isActive);
+}
+
+export default SvgIcon;
