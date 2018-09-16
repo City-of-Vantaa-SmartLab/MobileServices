796a1d405e9f251d46ba86f1a039e97099b064b5
Mariia Ustilkina <mariia.ustilkina@gofore.com>
2018-09-17 00:13:05 +0300

    Create news card skeleton

--- front-end/src/components/newsfeed-view/NewsfeedView.js
+++ front-end/src/components/newsfeed-view/NewsfeedView.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Backgrounds from './Backgrounds';
+import NewsList from './news-list/NewsList';
 
 class NewsfeedView extends Component {
+    state = {active: false}
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.activateFeed);
+    }
+
+    activateFeed = () => {
+        if (!this.state.active) {
+            this.setState({active: true})
+        }
+    }
 
     render() {
         return (
             <div>
                 <Backgrounds />
                 <Header />
+                <NewsList active={this.state}/>
+                
             </div>
         )
     }
