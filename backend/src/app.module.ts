72c950342f42123b55fa7867cde5568a34eebaa0
Shamnad Shaji <shamnad.shaji@gofore.com>
2018-09-18 20:18:14 +0300

    changes for getting facebook feeds

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,9 +5,15 @@ import { RssFeedModule } from './rss-feed/rss-feed.module'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionDetails } from './connection';
 import { YouTubeFeedModule } from './youtube-feed/youtube-feed.module';
+import { FacebookFeedModule } from './facebook-feed/facebook-feed.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionDetails), RssFeedModule, YouTubeFeedModule],
+  imports: [
+    TypeOrmModule.forRoot(connectionDetails),
+    RssFeedModule,
+    YouTubeFeedModule,
+    FacebookFeedModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
