abacb99d8b73ffe1493610b027a89f1332013aa4
biraj.paul <biraj.paul@gofore.com>
2018-09-20 16:54:26 +0300

    Styling of cookie container
    In this commit, the following things were done: Addition of GT-Walsheim fonts from the Cultural events project, Usage of Bootstrap Modal, Modification of the Modal using our own styles.
    Additionally, the css files in assets/stylesheets were removed as they do not need to be tracked.

--- front-end/src/components/cookie/CookieView.js
+++ front-end/src/components/cookie/CookieView.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
+import { Modal, Button } from "react-bootstrap";
+
 import "./CookieView.css";
 
 const isCookieSet = cookiePolicy => cookiePolicy && cookiePolicy === true ? true : false;
 
 class CookieView extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show: true
+        };
+    
+        this.handleClose = this.handleClose.bind(this);
+    }
+
+    handleClose() {
+        this.setState({ show: false });
+    }
 
     render() {
         const cookiePolicy = this.props.cookies.get('policy');
+        const showCookieDiv = !isCookieSet(cookiePolicy) && this.state.show;
 
         return (
-            <div className="cookie-view" style={{ display: isCookieSet(cookiePolicy) ? 'none' : 'block' }}>
-                <div>
-                    Please accept the cookies
-                </div>
-            </div>
+            <Modal show={showCookieDiv} onHide={this.handleClose} bsClass="cookie-modal">
+                <Modal.Header closeButton>
+                    <Modal.Title>Cookie Policy</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    The Vantaa Feed website uses cookies to ensure you receive the best experience on our awesome site! 
+                    You do not have to accept the cookie to view our service. Visit our 
+                    <a href="http://www.vantaa.fi/tietoa_sivustosta"> Privacy Policy</a> to learn more.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={this.handleClose}>ACCEPT</Button>
+                </Modal.Footer>
+            </Modal>
         )
     }
 
