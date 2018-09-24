d3c8af2c9bcb037c9a113e438b02a06b7d04b241
Thanh Nguyen <thanh.nguyen@gofore.com>
2018-09-24 15:17:27 +0300

    infra-dev: added several addons to storybook. Included some examples

--- front-end/src/components/navigation/navigation.stories.js
+++ front-end/src/components/navigation/navigation.stories.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Navigation from './NavigationPanel';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { withNotes, withMarkdownNotes } from '@storybook/addon-notes';
+
+let notes = `
+  # Navigation components!
+  ## Purpose
+  This is the bottom navigation component
+  ## How to use
+  This switches pages based on \`react-router-dom\`
+`
 
 storiesOf('Navigation', module)
   .addDecorator(story => (
@@ -9,6 +18,4 @@ storiesOf('Navigation', module)
       {story()}
     </BrowserRouter>
   ))
-  .add('with text', () => (
-    <Navigation />
-  ))
+  .add('with some notes', withMarkdownNotes(notes)(() => <Navigation />))
