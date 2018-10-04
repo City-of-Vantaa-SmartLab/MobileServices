7a63c594fe024f4093e4c91c963a82e2eeab725c
biraj.paul <biraj.paul@gofore.com>
2018-10-04 16:01:37 +0300

    Refactored instagram card

--- front-end/src/components/newsfeed-view/news-list/card-group/news-card/InstagramCard.js
+++ front-end/src/components/newsfeed-view/news-list/card-group/news-card/InstagramCard.js
@@ -3,18 +3,23 @@ import Timestamp from './elements/Timestamp';
 import styles from './social-media-card.module.scss';
 
 const InstagramCard = ({data}) => (
-    <div className={`${styles['news-card']} ${styles['vertical']} ${styles['instagram']}`}>
+    <div className={`${styles['social-media-card']} ${styles['instagram']}`}>
+        <div className={styles["service-source"]}>
+            {data.source}
+        </div>
         <div className={styles['content']}>
-            <div className={styles['source']}>
-                <div className={styles['author']}>
-                <img src = {data.thumbnail} />
-                 <p>{data.author}</p>
-                </div>
-                <span>{data.source}</span>
+            <div className={styles['content-source']}>
+                <img src={data.thumbnail} />
+                <div>{data.author}</div>
             </div>
-            <div className={styles['image']} style={{ backgroundImage: `url(${data.img})` }}></div>
+            <div className={styles['image']}>
+                <img src = {data.img} />
+            </div>
             <div className={styles['description']}>
-                <p><b>{data.author}</b> {data.description}</p>
+                <div class={styles['likes']}>{data.likes} likes</div>
+                <div>
+                    <b>{data.author}</b> {data.description}
+                </div>
             </div>
             <div className={styles['footer']}>
                 <Timestamp time = {data.timestamp} />
--- front-end/src/components/newsfeed-view/news-list/card-group/news-card/TwitterCard.js
+++ front-end/src/components/newsfeed-view/news-list/card-group/news-card/TwitterCard.js
@@ -3,7 +3,7 @@ import Timestamp from './elements/Timestamp';
 import styles from './social-media-card.module.scss';
 
 const TwitterCard = ({data}) => (
-    <div className={`${styles['news-card']} ${styles['vertical']} ${styles['twitter']}`}>
+    <div className={`${styles['social-media-card']} ${styles['vertical']} ${styles['twitter']}`}>
         <div className={styles['content']}>
             <div className={styles['source']}>
                 <div className={styles['author']}>
