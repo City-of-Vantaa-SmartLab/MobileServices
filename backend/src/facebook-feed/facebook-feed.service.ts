d69cbaae8ca83076df10ecd0c6ba58af562440f7
Shamnad Shaji <shamnad.shaji@gofore.com>
2018-10-10 09:08:43 +0300

    Changed facebook data fetch to fb sdk and fetch author details

--- backend/src/facebook-feed/facebook-feed.service.ts
+++ backend/src/facebook-feed/facebook-feed.service.ts
@@ -3,7 +3,9 @@ import { ConfigService } from '../config/config.service';
 import { sources, queryParams } from './facebook-utils';
 import { FeedService } from '../feeds/feed.service';
 const config = new ConfigService();
-const axios = require('axios');
+
+import FB from 'fb';
+FB.setAccessToken(config.facebookPageToken);
 
 @Injectable()
 export class FacebookFeedService {
@@ -19,34 +21,40 @@ export class FacebookFeedService {
     }
 
     async fetchFacebookFeeds() {
-        try {
-            this.logger.log('Fetching facebook feeds Started');
-            const vantaaFacebookDataUrl = `${config.facebookGraphApi}${sources.vantaa}/posts?`
-                + `fields=${queryParams.join(',')}`
-                + `&access_token=${config.facebookPageToken}`;
-            const facebookFeeds = this.transformData((await axios.get(vantaaFacebookDataUrl)).data.data);
-            await this.feedService.saveFeeds(facebookFeeds);
-            this.logger.log('Fetching facebook feeds Completed');
-        } catch (error) {
-            this.logger.error(`Failed to get facebook feeds: ${error}`)
-        }
+        this.logger.log('Fetching facebook feeds Started');
+        FB.api(`${sources.vantaa}/posts`, { fields: queryParams })
+            .then(this.transformData)
+            .then(this.persistIntoDb)
+            .catch(error => this.logger.error(`Failed to get facebook feeds: ${error}`));
+        this.logger.log('Fetching facebook feeds Completed');
     }
 
-    transformData = feeds => {
-        return feeds.map(feed => {
-            const feed_id = feed.id;
-            delete feed.id;
-            return {
-                ...feed,
-                comments: feed.comments ? feed.comments.data : null,
-                likes: feed.reactions ? feed.reactions.data.length : null,
-                description: feed.story ? feed.story : feed.message,
-                source: 'Facebook',
-                title: feed.status_type,
-                pub_date: feed.created_time,
-                image_url: feed.picture,
-                feed_id
-            }
-        });
+    persistIntoDb = feeds => this.feedService.saveFeeds(feeds);
+
+    transformData = async (feeds) => {
+        return await Promise.all(
+            feeds.data.map(async feed => {
+                let profileImage = await this.getProfileImage(feed.from.id);
+                const feed_id = feed.id;
+                delete feed.id;
+                return {
+                    author: feed.from.name,
+                    author_thumbnail: profileImage,
+                    likes: feed.reactions ? feed.reactions.data.length : null,
+                    description: feed.story ? feed.story : feed.message,
+                    source: 'Facebook',
+                    title: feed.status_type,
+                    pub_date: feed.created_time,
+                    image_url: feed.picture,
+                    feed_id
+                }
+            }));
+    }
+
+    getProfileImage = (id: number) => {
+        return FB.api(`${id}/picture`, {
+            "redirect": "0"
+        }).then(profile => profile.data.url)
+            .catch(error => this.logger.error(`Failed to get profile details: ${error}`));
     }
 }
