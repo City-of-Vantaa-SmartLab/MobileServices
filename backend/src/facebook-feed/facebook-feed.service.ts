77f6b7d56a1825b9a4c299cffb2261bc4483626b
Shamnad Shaji <shamnad.shaji@gofore.com>
2018-10-12 12:56:38 +0300

    updated transform and image fetch logic

--- backend/src/facebook-feed/facebook-feed.service.ts
+++ backend/src/facebook-feed/facebook-feed.service.ts
@@ -23,38 +23,55 @@ export class FacebookFeedService {
     async fetchFacebookFeeds() {
         this.logger.log('Fetching facebook feeds Started');
         FB.api(`${sources.vantaa}/posts`, { fields: queryParams })
+            .then(this.addProfileImage)
             .then(this.transformData)
-            .then(this.persistIntoDb)
+            .then((feeds) => {
+                this.persistIntoDb(feeds);
+                this.logger.log('Fetching facebook feeds Completed');
+            })
             .catch(error => this.logger.error(`Failed to get facebook feeds: ${error}`));
-        this.logger.log('Fetching facebook feeds Completed');
+
     }
 
     persistIntoDb = feeds => this.feedService.saveFeeds(feeds);
 
-    transformData = async (feeds) => {
+    addProfileImage = async feeds => {
         return await Promise.all(
             feeds.data.map(async feed => {
-                let profileImage = await this.getProfileImage(feed.from.id);
-                const feed_id = feed.id;
-                delete feed.id;
+                let profileImage;
+                try {
+                    profileImage = await this.fetchProfileImage(feed.from.id);
+                } catch (error) {
+                    this.logger.error(`Failed to get profile Image: ${error}`)
+                }
                 return {
-                    author: feed.from.name,
-                    author_thumbnail: profileImage,
-                    likes: feed.reactions ? feed.reactions.data.length : null,
-                    description: feed.story ? feed.story : feed.message,
-                    source: 'Facebook',
-                    title: feed.status_type,
-                    pub_date: feed.created_time,
-                    image_url: feed.picture,
-                    feed_id
+                    ...feed,
+                    author_thumbnail: profileImage ? profileImage.data.url : null
                 }
             }));
     }
 
-    getProfileImage = (id: number) => {
+    transformData = feeds => {
+        return feeds.map(feed => {
+            const feed_id = feed.id;
+            delete feed.id;
+            return {
+                author: feed.from.name,
+                author_thumbnail: feed.author_thumbnail,
+                likes: feed.reactions ? feed.reactions.data.length : null,
+                description: feed.story ? feed.story : feed.message,
+                source: 'Facebook',
+                title: feed.status_type,
+                pub_date: feed.created_time,
+                image_url: feed.picture,
+                feed_id
+            }
+        });
+    }
+
+    fetchProfileImage = (id: number) => {
         return FB.api(`${id}/picture`, {
             "redirect": "0"
-        }).then(profile => profile.data.url)
-            .catch(error => this.logger.error(`Failed to get profile details: ${error}`));
+        });
     }
 }
