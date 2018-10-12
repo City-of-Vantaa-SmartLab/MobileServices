faac4b74f3c84208df31043ef7962c848e87de56
biraj.paul <biraj.paul@gofore.com>
2018-10-12 14:42:22 +0300

    Added opacity transition for front page date and header

--- front-end/src/components/newsfeed-view/NewsfeedView.js
+++ front-end/src/components/newsfeed-view/NewsfeedView.js
@@ -4,13 +4,33 @@ import Backgrounds from './carousel/Backgrounds';
 import NewsList from './news-list/NewsList';
 
 class NewsfeedView extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            opacity: 1.0,
+        };
+        this.opacityScrollLimit = 150;
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        this.setState({ opacity: (this.opacityScrollLimit - window.scrollY) / this.opacityScrollLimit });
+    };
 
     render() {
         return (
             <div>
                 <Backgrounds />
-                <Header />
-                <NewsList/>
+                <Header opacity={this.state.opacity} />
+                <NewsList />
             </div>
         );
     }
