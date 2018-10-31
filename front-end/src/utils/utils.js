90f1e9dc4a7aa54f2b46129469d88c8f550151b0
biraj.paul <biraj.paul@gofore.com>
2018-10-31 15:00:35 +0200

    Added Events card

--- front-end/src/utils/utils.js
+++ front-end/src/utils/utils.js
@@ -3,18 +3,15 @@ import en from 'date-fns/locale/en';
 import fi from 'date-fns/locale/fi';
 import sv from 'date-fns/locale/sv';
 
-const locales = {en, fi, sv};
+const locales = { en, fi, sv };
 
-const formatDate = (date, lang) => (format(date, 'dddd, MMMM DD', {locale: locales[lang]}));
+const formatDate = (date, lang) => format(date, 'dddd, MMMM DD', { locale: locales[lang] });
 
-const getTimeDelta = (date, lang) => (
-  (differenceInDays(new Date(date), new Date()) > 1) ?
-    distanceInWordsToNow(new Date(date), {addSuffix: true, locale: locales[lang]}) 
-    : 
-    format(new Date(date), 'D MMMM, H:mm', {locale: locales[lang]})
-);
+const getTimeDelta = (date, lang) =>
+    differenceInDays(new Date(date), new Date()) > 1
+        ? distanceInWordsToNow(new Date(date), { addSuffix: true, locale: locales[lang] })
+        : format(new Date(date), 'D MMMM, H:mm', { locale: locales[lang] });
 
-export {
-  formatDate,
-  getTimeDelta,
-};
+const getEventsTime = (date, lang) => format(new Date(date), 'D MMMM, H:mm', { locale: locales[lang] });
+
+export { formatDate, getTimeDelta, getEventsTime };
