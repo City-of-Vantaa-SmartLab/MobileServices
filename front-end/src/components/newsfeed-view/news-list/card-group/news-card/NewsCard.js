016ee154837fcb078dcac7d2df68ebba02a1f99d
biraj.paul <biraj.paul@gofore.com>
2018-11-02 16:02:22 +0200

    News card refactor

--- front-end/src/components/newsfeed-view/news-list/card-group/news-card/NewsCard.js
+++ front-end/src/components/newsfeed-view/news-list/card-group/news-card/NewsCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Timestamp from './elements/Timestamp';
 import styles from './news-card.module.scss';
 import ExpandableContent from './elements/ExpandableContent';
-import img from 'assets/images/cutiepie.jpg';
+import img from 'assets/images/vantaa-logo.svg';
 import share_button from 'assets/images/share_button.svg';
 import copy_icon from 'assets/images/copy_icon.svg';
 import ToastNotification from './elements/ToastNotification';
@@ -11,30 +11,35 @@ import CardHOC from './CardHOC';
 class NewsCard extends React.Component {
     render() {
         let { data } = this.props;
+        console.log(data);
         return (
             <div className={styles['news-card']}>
                 <div className={styles['source']}>{data.source}</div>
-                <div className={styles['image']} style={{ backgroundImage: `url(${img})` }} />
                 <div className={styles['content']}>
-                    <div className={styles['description']}>
+                    <div className={styles['image']} style={{ backgroundImage: `url(${img})` }} />
+                    <div className={styles['title']}>
                         <span>Author / page headline</span>
-                        <ExpandableContent
-                            maxLine={3}
-                            description={data.title}
-                            ellipsis="..."
-                            lineHeight={1.9}
-                            className={styles['content-text']}
-                            ellipsisClickable={false}
-                        />
+                        <br />
+                        <div>{data.title}</div>
                     </div>
-                    <div className={styles['footer']}>
-                        <Timestamp time={data.pub_date} />
-                        <img
-                            src={navigator.share ? share_button : copy_icon}
-                            alt="Share button"
-                            onClick={() => this.props.handleClick(data.source, data.title)}
-                        />
-                    </div>
+                </div>
+                <div className={styles['description']}>
+                    <ExpandableContent
+                        maxLine={6}
+                        description={data.description}
+                        ellipsis="..."
+                        lineHeight={1.21}
+                        className={styles['content-text']}
+                        ellipsisClickable={false}
+                    />
+                </div>
+                <div className={styles['footer']}>
+                    <Timestamp time={data.pub_date} />
+                    <img
+                        src={navigator.share ? share_button : copy_icon}
+                        alt="Share button"
+                        onClick={() => this.props.handleClick(data.source, data.title)}
+                    />
                 </div>
                 {this.props.showNotification && <ToastNotification text="Copied to clipboard" />}
             </div>
