55e82bb77bded6480e1350914f58298bd0ef4012
biraj.paul <biraj.paul@gofore.com>
2018-11-02 16:57:30 +0200

    Added react-lines-ellipsis for better multiline text crop

--- front-end/src/components/newsfeed-view/news-list/card-group/news-card/NewsCard.js
+++ front-end/src/components/newsfeed-view/news-list/card-group/news-card/NewsCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Timestamp from './elements/Timestamp';
 import styles from './news-card.module.scss';
-import ExpandableContent from './elements/ExpandableContent';
+import LinesEllipsis from 'react-lines-ellipsis';
 import img from 'assets/images/vantaa-logo.svg';
 import share_button from 'assets/images/share_button.svg';
 import copy_icon from 'assets/images/copy_icon.svg';
@@ -9,6 +9,28 @@ import ToastNotification from './elements/ToastNotification';
 import CardHOC from './CardHOC';
 
 class NewsCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            maxLines: 6,
+        };
+        this.descriptionRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.observer = new MutationObserver(() => {
+            this.moreText = this.descriptionRef.current.getElementsByClassName('LinesEllipsis-ellipsis')[0];
+            if (this.moreText) {
+                this.moreText.addEventListener('click', () => {
+                    this.setState({ maxLines: 1000 });
+                });
+            }
+            this.observer.disconnect();
+        });
+        this.observer.observe(this.descriptionRef.current, { attributes: false, childList: true, subtree: true });
+    }
+
     render() {
         let { data } = this.props;
         console.log(data);
@@ -20,17 +42,18 @@ class NewsCard extends React.Component {
                     <div className={styles['title']}>
                         <span>Author / page headline</span>
                         <br />
-                        <div>{data.title}</div>
+                        <div>
+                            <LinesEllipsis text={data.title} maxLine="3" ellipsis="..." trimRight basedOn="letters" />
+                        </div>
                     </div>
                 </div>
-                <div className={styles['description']}>
-                    <ExpandableContent
-                        maxLine={6}
-                        description={data.description}
-                        ellipsis="..."
-                        lineHeight={1.21}
-                        className={styles['content-text']}
-                        ellipsisClickable={false}
+                <div ref={this.descriptionRef} className={styles['description']}>
+                    <LinesEllipsis
+                        text={data.description}
+                        maxLine={this.state.maxLines}
+                        ellipsis="... more"
+                        trimRight
+                        basedOn="letters"
                     />
                 </div>
                 <div className={styles['footer']}>
