f6b5a6c6d28ffd940a8b661a30f1c1774f7d5edc
Mariia Ustilkina <mariia.ustilkina@gofore.com>
2018-11-08 13:52:00 +0200

    Add dynamic transparency for main page header

--- front-end/src/components/newsfeed-view/Header.js
+++ front-end/src/components/newsfeed-view/Header.js
@@ -3,8 +3,8 @@ import { connect } from 'react-redux';
 import DateHeader from './DateHeader';
 import styles from './header.module.scss';
 
-const Header = ({ i18n }) => (
-    <div className={styles['header']}>
+const Header = ({ i18n, opacity }) => (
+    <div className={styles['header']} style={{ opacity: opacity }}>
         <h1>{i18n.newsfeed.header}</h1>
         <DateHeader />
     </div>
--- front-end/src/components/newsfeed-view/NewsfeedView.js
+++ front-end/src/components/newsfeed-view/NewsfeedView.js
@@ -4,13 +4,33 @@ import Backgrounds from './carousel/Backgrounds';
 import NewsListContainer from './news-list/NewsListContainer';
 
 class NewsfeedView extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            opacity: 1.0,
+        };
+        this.opacityScrollLimit = 500;
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        this.setState({ opacity: (this.opacityScrollLimit - window.scrollY) / this.opacityScrollLimit });
+    };
 
     render() {
         return (
             <div>
                 <Backgrounds />
-                <Header />
-                <NewsListContainer/>
+                <Header opacity={this.state.opacity} />
+                <NewsListContainer />
             </div>
         );
     }
