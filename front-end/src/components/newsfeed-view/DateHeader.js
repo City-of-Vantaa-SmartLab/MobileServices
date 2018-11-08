e208add858dc6fd6488e309511496cf95f21280e
Mariia Ustilkina <mariia.ustilkina@gofore.com>
2018-11-08 13:47:50 +0200

    Add sticky date header

--- front-end/src/components/newsfeed-view/DateHeader.js
+++ front-end/src/components/newsfeed-view/DateHeader.js
@@ -3,19 +3,41 @@ import { connect } from 'react-redux';
 import { formatDate } from 'utils/utils';
 import styles from './header.module.scss';
 
-const DateHeader = ({timestamp, i18n}) => {
-    let date = timestamp ? new Date(timestamp) : new Date();
-    return (
-        <div className={styles['date']}>
-            <h4>{formatDate(date, i18n.locale)}</h4>
-        </div>
-    );
-};
+class DateHeader extends React.Component {
+    constructor(props) {
+        super(props);
+        this.headerRef = React.createRef();
+    }
 
-const mapStateToProps = state => ({
+    componentDidMount() {
+        if (this.props.newsfeedHeader) window.addEventListener('NewsFeedContainerScrolling', this.handleScroll);
+    }
+
+    componentWillUnmount() {
+        if (this.props.newsfeedHeader) window.removeEventListener('NewsFeedContainerScrolling', this.handleScroll);
+    }
+
+    handleScroll = () => {
+        this.headerRef.current.style['width'] =
+            this.headerRef.current.getBoundingClientRect().top < 50 ? '100%' : 'auto';
+    };
+
+    render() {
+        let date = this.props.timestamp ? new Date(this.props.timestamp) : new Date();
+
+        return (
+            <div
+                className={`${styles['date']} ${this.props.newsfeedHeader ? styles['newsfeed-date'] : ''} `}
+                ref={this.headerRef}
+            >
+                <h4>{formatDate(date, this.props.i18n.locale)}</h4>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
     i18n: state.i18n,
 });
 
-export default connect(
-    mapStateToProps,
-)(DateHeader);
+export default connect(mapStateToProps)(DateHeader);
