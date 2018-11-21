5e0b33b45e7168ec3b12c4412382b74e01026f04
Mariia Ustilkina <mariia.ustilkina@gofore.com>
2018-11-21 23:53:50 +0200

    Update all feed selection buttons to blue

--- front-end/src/components/settings-view/Checkbox.js
+++ front-end/src/components/settings-view/Checkbox.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import styles from './settings.module.scss';
 
-
 const Checkbox = (props) => (
-    <a 
-        href='' 
-        onClick={e => {
+    <a
+        href=""
+        onClick={(e) => {
             e.preventDefault();
             props.onClick();
-            }}  
-        className={`${props.active ? styles['active']:''} ${styles[props.colorTag]}` }>
+        }}
+        className={props.active ? styles['active'] : ''}
+    >
         {props.children}
     </a>
- );
+);
 
- export default Checkbox;
+export default Checkbox;
--- front-end/src/components/settings-view/FeedCheckboxController.js
+++ front-end/src/components/settings-view/FeedCheckboxController.js
@@ -2,11 +2,11 @@ import { connect } from 'react-redux';
 import { toggleFeed } from 'actions';
 import Checkbox from './Checkbox';
 
-const colorTags = ['instagram', 'twitter', 'facebook', 'youtube'];
+//const colorTags = ['instagram', 'twitter', 'facebook', 'youtube'];
 
 const mapStateToProps = (state, ownProps) => ({
     active: state.feedTypes[ownProps.type],
-    colorTag: colorTags.indexOf(ownProps.type) !== -1 ? ownProps.type : '',
+    //colorTag: colorTags.indexOf(ownProps.type) !== -1 ? ownProps.type : '',
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => {
